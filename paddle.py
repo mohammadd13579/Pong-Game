0f543592e4b16c318357a11770bb60f2fbb11cd7
𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <83270567+mohammadd13579@users.noreply.github.com>
2024-04-06 18:25:39 +0330

    Add files via upload

--- paddle.py
+++ paddle.py
@@ -12,9 +12,11 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() < 250:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() > -230:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
